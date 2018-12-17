b68a41fb3ff5f94e35d818633b1da6b74566d542
Sai Osore <sai.osore@gmail.com>
2018-12-17 14:21:45 +0200

    waxom update

--- portfolio/Waxom/js/main.js
+++ portfolio/Waxom/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 	//Slick Slider
     $('.slider-main__slides').slick({
     	dots: true,
@@ -16,34 +15,13 @@ $(document).ready(function(){
         nextArrow: '.recent-post-slider__next',
         prevArrow: '.recent-post-slider__prev',
 	});
-
     //menu
     $('.hamburger').on('click', function() {
-       $('.nav').slideToggle('fast', function() {
-            if( $(this).css('display') === "none") {
-                $(this).removeAttr('style');}
-        $('.hamburger').toggleClass('is-active');
+       $('.nav-side').slideToggle('fast', function(){
+            if( $(this).css('display') === "none"){
+                $(this).removeAttr('style');
+            }
+            $('.hamburger').toggleClass('is-active');
        });
     });
-
-    //counter
-   $('.counters__count').each(function() {
-      $(this).prop('Counter', 0).animate( {
-        Counter: $(this).text() }, 
-        {
-        duration: 2000,
-        easing: 'swing',
-        step: function(now) {
-          $(this).text(Math.ceil(now));
-        }
-      });
-    });
-
-    //search
-    $('.form-search').on('click', function() {
-        if ( $('.form-search__input').css('display') === "none") 
-        {
-         $('.form-search__input').css('display', 'inline-block');
-        };
-   });
 });
