b3700477e2b185dd808702535a2c64d1bf9db70d
Sai Osore <sai.osore@gmail.com>
2019-10-02 14:10:25 +0300

    Update main.js

--- portfolio/test/js/main.js
+++ portfolio/test/js/main.js
@@ -258,7 +258,7 @@ var locationPathName = location.pathname;
   var logoLinkJs = document.querySelector('.logoLinkJs');
   var propertyLink = document.getElementById('propertyLinkAnchor');
 
-  if (locationPathName === '/portfolio/test') {
+  if (locationPathName === '/portfolio/test/') {
     logoLinkJs.href = 'javascript:void(0);';
     var linkNav = document.querySelectorAll('[href^="./#"]');
     console.log(linkNav);
