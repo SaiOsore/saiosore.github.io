e32db3b257d5ad405f6a0160d159e30ab3825746
Sai Osore <sai.osore@gmail.com>
2019-12-03 01:44:35 +0200

    home page main section fix, text editing

--- portfolio/test3/js/main.js
+++ portfolio/test3/js/main.js
@@ -240,61 +240,75 @@ if (centerVideo) {
     });
   }
 })();
-// class TypeWriter {
-//   constructor(txtElement, words, wait = 3000) {
-//     this.txtElement = txtElement;
-//     this.words = words;
-//     this.txt = '';
-//     this.wordIndex = 0;
-//     this.wait = parseInt(wait, 10);
-//     this.type();
-//     this.isDeleting = false;
-//   }
-//   type() {
-//     // Current index of word
-//     const current = this.wordIndex % this.words.length;
-//     // Get full text of current word
-//     const fullTxt = this.words[current];
-//     // Check if deleting
-//     if(this.isDeleting) {
-//       // Remove char
-//       this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//       // Add char
-//       this.txt = fullTxt.substring(0, this.txt.length + 1);
-//     }
-//     // Insert txt into element
-//     this.txtElement.innerHTML = `<span class="txt">${this.txt}<span class="txt-border">|</span></span>`;
-//     // Initial Type Speed
-//     let typeSpeed = 300;
-//     if(this.isDeleting) {
-//       typeSpeed /= 2;
-//     }
-//     // If word is complete
-//     if(!this.isDeleting && this.txt === fullTxt) {
-//       // Make pause at end
-//       typeSpeed = this.wait;
-//       // Set delete to true
-//       this.isDeleting = true;
-//     } else if(this.isDeleting && this.txt === '') {
-//       this.isDeleting = false;
-//       // Move to next word
-//       this.wordIndex++;
-//       // Pause before start typing
-//       typeSpeed = 500;
-//     }
-//     setTimeout(() => this.type(), typeSpeed);
-//   }
-// }
-// // Init On DOM Load
-// document.addEventListener('DOMContentLoaded', init);
-// // Init App
-// function init() {
-//   const txtElement = document.querySelector('.txt-type');
-//   const words = JSON.parse(txtElement.getAttribute('data-words'));
-//   const wait = txtElement.getAttribute('data-wait');
-//   // Init TypeWriter
-//   new TypeWriter(txtElement, words, wait);
-// }
 "use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var TypeWriter =
+/*#__PURE__*/
+function () {
+  function TypeWriter(txtElement, words) {
+    var wait = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 3000;
+
+    _classCallCheck(this, TypeWriter);
+
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  _createClass(TypeWriter, [{
+    key: "type",
+    value: function type() {
+      var _this = this;
+
+      var current = this.wordIndex % this.words.length;
+      var fullTxt = this.words[current];
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.txtElement.innerHTML = "<span class=\"txt\">".concat(this.txt, "<span class=\"txt-border\">|</span></span>");
+      var typeSpeed = 300;
+
+      if (this.isDeleting) {
+        typeSpeed /= 2;
+      }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        typeSpeed = this.wait;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        typeSpeed = 1000;
+      }
+
+      setTimeout(function () {
+        return _this.type();
+      }, typeSpeed);
+    }
+  }]);
+
+  return TypeWriter;
+}();
+
+var txtElement = document.querySelector('.txtAnimation');
+
+if (txtElement) {
+  var words = JSON.parse(txtElement.getAttribute('data-words'));
+  var wait = txtElement.getAttribute('data-wait');
+  new TypeWriter(txtElement, words, wait);
+}
 //# sourceMappingURL=maps/main.js.map
